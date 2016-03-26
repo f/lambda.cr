997e581743a3a7a7384f631e815e3fc9c41b9a75
RX14 <chris@rx14.co.uk>
2016-03-26 17:17:21 +0000

    Add new lambda syntax using normal method definition

--- src/lambda.cr
+++ src/lambda.cr
@@ -1,6 +1,30 @@
 require "./lambda/*"
 
-macro λ(name, &block)
+macro lambda_method(method)
+  {% if method.args.size == 0 %}
+    raise "Uniformed functions have to have at least one parameter."
+  {% end %}
+
+  {% args = method.args.argify %}
+
+  {% if method.args.size > 1 %}
+    {% args = args.gsub(/^.+?, /, "") %}
+  {% else %}
+    {% args = "" %}
+  {% end %}
+
+  # Define original method
+  {{method.id}}
+
+  class Object
+    def {{method.name.id}}({{args.id}})
+      {{method.args.first.name}} = self
+      {{method.body}}
+    end
+  end
+end
+
+macro lambda_block(name, &block)
   {% if block.args.size == 0 %}
     raise "Uniformed functions have to have at least one parameter."
   {% end %}
@@ -22,6 +46,14 @@ macro λ(name, &block)
   def {{name.id}}({{block.args.first.id.gsub(/_as_/, " : ")}},{{args.id}})
     {{block.body}}
   end
+end
+
+macro λ(name, &block)
+  {% if name.is_a?(Def) %}
+    lambda_method {{name}}
+  {% else %}
+    lambda_block {{name}} {{block}}
+  {% end %}
 end
 
 macro lambda(name, &block)
